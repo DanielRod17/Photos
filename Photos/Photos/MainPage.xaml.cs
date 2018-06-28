b99ecb01e2de8bdbf2e5773d06edb9825081f0bf
Daniel Rodríguez <daniel.rod.vega@hotmail.com>
2018-06-28 09:23:51 -0600

    Photos

--- Photos/Photos/MainPage.xaml.cs
+++ Photos/Photos/MainPage.xaml.cs
@@ -3,15 +3,88 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Plugin.Media;
 using Xamarin.Forms;
+using Plugin.Permissions.Abstractions;
+using Plugin.Permissions;
 
 namespace Photos
 {
     public partial class MainPage : ContentPage
     {
+       
         public MainPage()
         {
             InitializeComponent();
+            var Image1 = new Image
+            {
+                HeightRequest = 240,
+                BackgroundColor = Color.Black
+            };
+        }
+
+        private async void UploadPictureButton_Clicked(object sender, EventArgs e)
+        {
+            if (!CrossMedia.Current.IsPickPhotoSupported)
+            {
+                await DisplayAlert("No upload", "Picking a phot is not supported.", "Ok");
+                return;
+            }
+            var file = await CrossMedia.Current.PickPhotoAsync();
+            if (file == null)
+                return;
+
+            Image1.Source = ImageSource.FromStream(() => file.GetStream());
+        }
+
+        private async void TakePictureButton_Clicked(object sender, EventArgs e)
+        {
+            var cameraStatus = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.Camera);
+            var storageStatus = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.Storage);
+            if (cameraStatus != PermissionStatus.Granted || storageStatus != PermissionStatus.Granted)
+            {
+                var results = await CrossPermissions.Current.RequestPermissionsAsync(new[] { Permission.Camera, Permission.Storage });
+                cameraStatus = results[Permission.Camera];
+                storageStatus = results[Permission.Storage];
+            }
+
+            if (cameraStatus == PermissionStatus.Granted && storageStatus == PermissionStatus.Granted)
+            {
+                var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+                {
+                    Directory = "Sample",
+                    Name = "test.jpg"
+                });
+            }
+            else
+            {
+                await DisplayAlert("Permissions Denied", "Unable to take photos.", "OK");
+                //On iOS you may want to send your user to the settings screen.
+                //CrossPermissions.Current.OpenAppSettings();
+            }
+            ////////////////////////////////////
+            /*if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await DisplayAlert("No Camera", ":( No camera available.", "OK");
+                return;
+            }
+
+            var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+            {
+                Directory = "Test",
+                Name = "test.jpg"
+            });
+
+            if (file == null)
+                return;
+
+            await DisplayAlert("File Location", file.Path, "OK");
+
+            Image1.Source = ImageSource.FromStream(() =>
+            {
+                var stream = file.GetStream();
+                return stream;
+            });*/
         }
     }
 }
